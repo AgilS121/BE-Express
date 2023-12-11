7a7b4fe4213a7b4d83bc47880f8f0ef09dfed927
AgilS121 <gilsatria121@gmail.com>
2023-12-12 06:15:01 +0700

    test

--- index.js
+++ index.js
@@ -3,36 +3,35 @@ import db from "./config/Database.js";
 import router from "./routes/index.js";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
-import cors from "cors"
-import dotenv from "dotenv"
-dotenv.config()
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const start = async function(a, b) {
+const start = async function (a, b) {
   try {
-    await db.authenticate()
-    console.log('Database Connected')
+    await db.authenticate();
+    console.log("Database Connected");
   } catch (e) {
-      console.log(e)
+    console.log(e);
   }
-}
+};
 
-app.use(cors({ credentials: true, origin: 'https://vite-react-donor-oop782wwz-agilsaps-projects.vercel.app' }));
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-  next()
-})
-app.use(cookieParser())
-app.use(express.json())
-app.use(fileUpload())
-app.use(express.static("public"))
-app.use(cors());
+const corsOptions = {
+  origin: "https://vite-react-donor-oop782wwz-agilsaps-projects.vercel.app",
+  methods: "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+  credentials: true, // enable set cookie
+  optionsSuccessStatus: 204,
+  allowedHeaders: "Content-Type, Authorization",
+};
 
-app.options('*', cors());
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(express.json());
+app.use(fileUpload());
+app.use(express.static("public"));
 
 app.use(router);
 
@@ -42,4 +41,4 @@ app.listen(port, () => {
   console.log(`Server berjalan di port ${port}`);
 });
 
-export default app
+export default app;
