fc7e9ff421f2fb4bb7931a8214fefe4fcd257567
AgilS121 <gilsatria121@gmail.com>
2023-12-12 05:08:03 +0700

    test

--- config/Database.js
+++ config/Database.js
@@ -1,25 +1,7 @@
 import { Sequelize } from "sequelize"
-const db = new Sequelize('donors', 'root',  '', {
-    host: "localhost",
+const db = new Sequelize(process.env.DB_DBNAME, process.env.DB_USERNAME,  process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: "mysql"
 })
 
 export default db
-// import mysql from 'mysql2'
-
-// const db = mysql.createPool({
-//     host: process.env.DB_HOST, 
-//     user: process.env.DB_USERNAME, 
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_DBNAME,
-//     waitForConnections: true,
-//     connectionLimit: 10,
-//     queueLimit: 0
-// });
-
-// db.getConnection((err, conn) => {
-//     if(err) console.log(err)
-//     console.log("Connected successfully")
-// })
-
-// export default db
